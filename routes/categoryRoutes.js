0bfeca8d59381be94a6f10810f4f13256b8e4501
edaniel-valencia <edanielvalenciam@gmail.com>
2024-07-14 10:28:56 -0500

    cambi

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,7 +3,8 @@ const passport = require('passport');
 
 module.exports = (app, upload) => {
 
-    app.get('/api/categories/getAll', passport.authenticate('jwt', {session: false}), categoriesController.getAll);
+    // app.get('/api/categories/getAll', passport.authenticate('jwt', {session: false}), categoriesController.getAll);
+    app.get('/api/categories/getAll', categoriesController.getAll);
     app.post('/api/categories/create', passport.authenticate('jwt', {session: false}) , upload.array('image', 1), categoriesController.create);
     app.put('/api/categories/updateWithImage', passport.authenticate('jwt', {session: false}) , upload.array('image', 1), categoriesController.updateWithImage);
     app.put('/api/categories/update', passport.authenticate('jwt', {session: false}) , categoriesController.update);
